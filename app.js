9debe2f9518da22ed4784664314510c342134384
Subhranshu-Lenka <s.lenka.official2022@gmail.com>
2024-01-01 13:03:55 +0530

    added scroll functionality

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const cursor = document.querySelector(".cursor");
 window.addEventListener("mousemove", (e) => {
     cursor.style.left = e.pageX + 'px';
     cursor.style.top = e.pageY + 'px';
-
+    cursor.setAttribute("data-fromTop", (cursor.offsetTop - scrollY));
     // console.log(e);
 });
+
+window.addEventListener("scroll", () => {
+    // console.log(e);
+    const fromTop = parseInt(cursor.getAttribute("data-fromTop"));
+    cursor.style.top = scrollY + fromTop + "px";
+})
